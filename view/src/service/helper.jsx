6cccce972ec36533cf0b01121d5c2fc5d45f6257
surajaswal-29 <suraj.aswal@quadbtech.com>
2024-04-24 23:53:10 +0530

    View Updated

--- view/src/service/helper.jsx
+++ view/src/service/helper.jsx
@@ -1,6 +1,6 @@
 // Localhost URI
 export const MAIN_URI =
-  window.location.hostname === "localhost"
+  import.meta.env.DEV
     ? "http://localhost:4000"
     : "https://themenhood.onrender.com";
 
