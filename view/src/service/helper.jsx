90a21d276feb8dc0388524d921f87a41294b5244
surajaswal29 <surajaswal29@gmail.com>
2024-04-25 09:53:30 +0530

    View Updated

--- view/src/service/helper.jsx
+++ view/src/service/helper.jsx
@@ -2,7 +2,7 @@
 export const MAIN_URI =
   import.meta.env.DEV
     ? "http://localhost:4000"
-    : "https://themenhood.onrender.com";
+    : "https://ecommerce-app-i3h2.onrender.com/";
 
 // Production URI
-export const FRONT_MAIN_URI = "https://themenhood.netlify.com";
+export const FRONT_MAIN_URI = "https://ecommerce-app-i3h2.onrender.com";
